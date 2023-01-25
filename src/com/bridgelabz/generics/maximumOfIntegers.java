3f22d36fbf057f4426cc4acdbe280c5902c3c19a
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-01-25 16:04:16 +0530

    added : program to find maximum of three integers using generics

---
+++ src/com/bridgelabz/generics/maximumOfIntegers.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.generics;
+
+public class maximumOfIntegers<T> {
+    T num1, num2, num3;
+
+    public static <T extends Comparable<T>> T maximumOfIntegers(T num1, T num2, T num3) {
+
+        T maximumNumber = num1;
+        if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
+            maximumNumber = num1;
+        else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
+            maximumNumber = num2;
+        else
+            maximumNumber = num3;
+        return maximumNumber;
+    }
+    // print maximum of methods
+
+    public static void main(String[] args) {
+        System.out.println("The max Number of Three positive numbers is:-\n "+maximumOfIntegers(65, 101, 1));
+        System.out.println("The max number of Three Float numbers is:-\n " + maximumOfIntegers(41.9f, 56.8f, 11.7f));
+    }
+}
